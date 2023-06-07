f6196451ebcb61c7e453c71f67c526c64520d2b9
Tech0tron <tech0tron.roblox@gmail.com>
2023-06-07 09:52:48 -0600

    Hello suzanne! - basic 3d rendering
    Model3d loading, perspective/view/model matrices, depth buffer

--- resources/shader.wgsl
+++ resources/shader.wgsl
@@ -1,3 +1,11 @@
+struct Uniforms {
+    perspective: mat4x4<f32>,
+    view: mat4x4<f32>,
+    model: mat4x4<f32>
+}
+
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
 struct VertexInput {
     @location(0) position: vec3<f32>
 }
@@ -11,9 +19,9 @@ struct VertexOutput {
 fn vs_main(in: VertexInput) -> VertexOutput {
     var out: VertexOutput;
     var ratio: f32 = 640.0 / 480.0;
-
-    out.color = vec3<f32>(1.0,0.0,0.0);
-    out.position = vec4<f32>(in.position.xyz, 1.0);
+    
+    out.color = vec3<f32>(in.position.xyz);
+    out.position = uniforms.perspective * uniforms.view * uniforms.model * vec4<f32>(in.position.xyz, 1.0);
 
 	return out;
 }
--- src/renderer.zig
+++ src/renderer.zig
@@ -1,7 +1,16 @@
 const std = @import("std");
 const gpu = @import("gpu");
+const zmath = @import("zmath");
 
-const Object = @import("obj_loader.zig").Object;
+const Mat = zmath.Mat;
+
+const Model = @import("loader.zig").Model;
+
+const Uniforms = struct {
+    perspective: Mat,
+    view: Mat,
+    model: Mat
+};
 
 const Renderer = @This();
 const log = std.log.scoped(.oceanman);
@@ -11,6 +20,10 @@ surface: *gpu.Surface,
 swapchain: *gpu.SwapChain,
 queue: *gpu.Queue,
 pipeline: *gpu.RenderPipeline,
+
+uniform_binding: *gpu.BindGroup,
+
+depth_texture: *gpu.Texture,
 
 vertex_buffer: *gpu.Buffer,
 vertex_count: usize,
@@ -32,9 +45,9 @@ pub fn init(gpa: std.mem.Allocator, device: *gpu.Device, surface: *gpu.Surface) 
         .present_mode = gpu.PresentMode.fifo,
         .format = gpu.Texture.Format.bgra8_unorm
     });
+    const ratio: f32  = 640.0 / 480.0;
 
-    var model = Object.createFromFile(gpa, "resources/triangle.obj");
-    _ = model;
+    var model = Model.createFromFile(gpa, "resources/suzanne.m3d") catch unreachable;
     
     // Write vertex and index buffers
     var vertex_buffer = device.createBuffer(&.{
@@ -42,44 +55,73 @@ pub fn init(gpa: std.mem.Allocator, device: *gpu.Device, surface: *gpu.Surface) 
             .vertex = true,
             .copy_dst = true
         },
-        .size = 24 * @sizeOf(f32)
+        .size = model.vertices.len * @sizeOf(f32)
     });
-    var vertex_count: usize = 8;
-    var slice = [_]f32{
-        0.999999, -0.999999, -0.999999,
-        0.999999, -0.999999, 0.999999,
-        -0.999999, -0.999999, 0.999999,
-        -0.999999, -0.999999, -0.999999,
-        0.999999, 0.999999, -0.999999,
-        0.999999, 0.999999, 0.999999,
-        -0.999999, 0.999999, 0.999999,
-        -0.999999, 0.999999, -0.999999
-    };
-    queue.writeBuffer(vertex_buffer, 0, &slice);
+    queue.writeBuffer(vertex_buffer, 0, model.vertices);
 
     var index_buffer = device.createBuffer(&.{
         .usage = gpu.Buffer.UsageFlags {
             .index = true,
             .copy_dst = true
         },
-        .size = 36 * @sizeOf(u32)
+        .size = model.indices.len * @sizeOf(u32)
     });
-    var index_count: usize = 36;
-    var index_slice = [_]u32{
-        1, 2, 3,
-        7, 6, 5,
-        4, 5, 1,
-        5, 6, 2,
-        2, 6, 7,
-        0, 3, 7,
-        0, 1, 3,
-        4, 7, 5,
-        0, 4, 1,
-        1, 5, 2,
-        3, 2, 7,
-        4, 0, 7,
+    queue.writeBuffer(index_buffer, 0, model.indices);
+
+    // Write uniform buffers and binding group.
+    var uniforms = Uniforms {
+        .model = zmath.mul(zmath.translation(0.0, 0.0, 0.0), zmath.scaling(0.5,0.5,0.5)),
+        .view = zmath.lookAtLh(zmath.f32x4(2.0, 0.0, 3.0, 1.0), zmath.f32x4(0.0, 0.0, 0.0, 1.0), zmath.f32x4(0.0, 1.0, 0.0, 0.0) ),
+        .perspective = zmath.perspectiveFovLh(0.4, ratio, 0.01, 100.0)
     };
-    queue.writeBuffer(index_buffer, 0, &index_slice);
+    var uniform_buffer = device.createBuffer(&.{
+        .usage = gpu.Buffer.UsageFlags {
+            .uniform = true,
+            .copy_dst = true
+        },
+        .size = @sizeOf(Uniforms)
+    });
+
+    var uniforms_slice: []Uniforms = undefined;
+    uniforms_slice.len = 1;
+    uniforms_slice.ptr = @ptrCast([*]Uniforms, &uniforms);
+    queue.writeBuffer(uniform_buffer, 0, uniforms_slice);
+
+    var uniform_layout = device.createBindGroupLayout(&gpu.BindGroupLayout.Descriptor.init(.{
+        .entries = &.{
+            gpu.BindGroupLayout.Entry.buffer(
+                    0, 
+                    gpu.ShaderStageFlags {
+                        .vertex = true
+                    },
+                    gpu.Buffer.BindingType.uniform,
+                    false,
+                    @sizeOf(Uniforms))
+        }
+    }));
+    var uniform_binding = device.createBindGroup(&gpu.BindGroup.Descriptor.init(.{
+        .layout = uniform_layout,
+        .entries = &.{
+            gpu.BindGroup.Entry.buffer(0, uniform_buffer, 0, @sizeOf(Uniforms)),
+        }
+    }));
+
+    // Depth texture
+    const depth_texture = device.createTexture(&gpu.Texture.Descriptor.init(.{
+        .usage = gpu.Texture.UsageFlags {
+            .render_attachment = true
+        },
+        .dimension = gpu.Texture.Dimension.dimension_2d,
+        .format = gpu.Texture.Format.depth24_plus,
+        .size = gpu.Extent3D {
+            .depth_or_array_layers = 1,
+            .width = 640,
+            .height = 480
+        },
+        .view_formats = &.{
+            gpu.Texture.Format.depth24_plus
+        },
+    }));
 
     // Render pipeline
     // FIXME: this cannot be the best way to add a sentinel
@@ -96,7 +138,9 @@ pub fn init(gpa: std.mem.Allocator, device: *gpu.Device, surface: *gpu.Surface) 
     defer shader_module.release();
 
     var pipeline = device.createRenderPipeline(&gpu.RenderPipeline.Descriptor {
-        .layout = device.createPipelineLayout(&gpu.PipelineLayout.Descriptor.init(.{})),
+        .layout = device.createPipelineLayout(&gpu.PipelineLayout.Descriptor.init(.{
+            .bind_group_layouts = &.{ uniform_layout }
+        })),
         .vertex = gpu.VertexState.init(.{
             .module = shader_module,
             .entry_point = "vs_main",
@@ -128,7 +172,13 @@ pub fn init(gpa: std.mem.Allocator, device: *gpu.Device, surface: *gpu.Surface) 
             }
         }),
         .primitive = .{},
-        .depth_stencil = null,
+        .depth_stencil = &.{
+            .format = gpu.Texture.Format.depth24_plus,
+            .depth_compare = gpu.CompareFunction.less,
+            .depth_write_enabled = true,
+            .stencil_read_mask = 0,
+            .stencil_write_mask = 0
+        },
         .multisample = .{}
     });
 
@@ -139,10 +189,12 @@ pub fn init(gpa: std.mem.Allocator, device: *gpu.Device, surface: *gpu.Surface) 
         .swapchain = swapchain,
         .queue = queue,
         .pipeline = pipeline,
+        .uniform_binding = uniform_binding,
+        .depth_texture = depth_texture,
         .vertex_buffer = vertex_buffer,
-        .vertex_count = vertex_count,
+        .vertex_count = model.vertices.len,
         .index_buffer = index_buffer,
-        .index_count = index_count
+        .index_count = model.indices.len
     };
 
 }
@@ -163,14 +215,30 @@ pub fn update(this: *Renderer) void {
                 .view = next_texture
             }
         },
+        .depth_stencil_attachment = &gpu.RenderPassDepthStencilAttachment {
+            .view = this.depth_texture.createView(&gpu.TextureView.Descriptor {
+                .aspect = gpu.Texture.Aspect.depth_only,
+                .base_array_layer = 0,
+                .array_layer_count = 1,
+                .base_mip_level = 0,
+                .mip_level_count = 1,
+                .dimension = gpu.TextureView.Dimension.dimension_2d,
+                .format = gpu.Texture.Format.depth24_plus
+            }),
+            .depth_clear_value = 1.0,
+            .depth_load_op = gpu.LoadOp.clear,
+            .depth_store_op = gpu.StoreOp.store,
+            .depth_read_only = false
+        }
     }));
     defer renderPass.release();
 
     renderPass.setPipeline(this.pipeline);
-    renderPass.setVertexBuffer(0, this.vertex_buffer, 0, this.vertex_count * 3 * @sizeOf(f32));
+    renderPass.setBindGroup(0, this.uniform_binding, null);
+    renderPass.setVertexBuffer(0, this.vertex_buffer, 0, this.vertex_count * @sizeOf(f32));
     renderPass.setIndexBuffer(this.index_buffer, gpu.IndexFormat.uint32, 0, this.index_count * @sizeOf(u32));
 
-    renderPass.drawIndexed(36, 1, 0, 0, 0);
+    renderPass.drawIndexed(@intCast(u32, this.index_count), 1, 0, 0, 0);
     renderPass.end();
         
     var commands = encoder.finish(&.{});
