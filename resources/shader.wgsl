e47a4d3173ecdd9fbe6d88a9edc506f2c87d7a50
Tech0tron <tech0tron.roblox@gmail.com>
2023-06-09 15:55:37 -0600

    wip: lighting, removing index buffers, fixing camera

--- resources/shader.wgsl
+++ resources/shader.wgsl
@@ -7,26 +7,33 @@ struct Uniforms {
 @group(0) @binding(0) var<uniform> uniforms: Uniforms;
 
 struct VertexInput {
-    @location(0) position: vec3<f32>
+    @location(0) position: vec3<f32>,
+    @location(1) normal: vec3<f32>
 }
 
 struct VertexOutput {
-    @builtin(position) position: vec4<f32>,
-    @location(0) color: vec3<f32>
+    @builtin(position) clip_space_position: vec4<f32>,
+    @location(0) world_position: vec3<f32>,
+    @location(1) normal: vec3<f32>
 }
 
 @vertex
 fn vs_main(in: VertexInput) -> VertexOutput {
     var out: VertexOutput;
-    var ratio: f32 = 640.0 / 480.0;
     
-    out.color = vec3<f32>(in.position.xyz);
-    out.position = uniforms.perspective * uniforms.view * uniforms.model * vec4<f32>(in.position.xyz, 1.0);
-
+    out.normal = in.normal;
+    out.clip_space_position = uniforms.perspective * uniforms.view * uniforms.model * vec4<f32>(in.position.xyz, 1.0);
+    out.world_position = in.position;
 	return out;
 }
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4f {
-    return vec4<f32>(in.color, 1.0);
+    var light_origin = vec3<f32>(-1.0, 10.0, 20.0);
+    var light_color = vec3<f32>(1.0, 0.0, 0.0);
+    var light_dir = normalize(light_origin - in.world_position);
+
+    var surface_color = vec3<f32>(1.0, 1.0, 1.0);
+    var color = dot(light_dir, normalize(in.normal)) * light_color * surface_color; 
+    return vec4<f32>( color, 1.0);
 }
--- src/loader.zig
+++ src/loader.zig
@@ -2,8 +2,7 @@ const std = @import("std");
 const model3d = @import("model3d");
 
 pub const Model = struct {
-    vertices: []f32,
-    indices: []u32,
+    buffer: []f32,
 
     pub fn createFromFile(gpa: std.mem.Allocator, path: []const u8) !Model {
         // FIXME: cmon man - sentinel b.s
@@ -27,17 +26,21 @@ pub const Model = struct {
             try vertices_toreturn.append(vertex.z);
         }
 
-        const indices: []model3d.Face = model.handle.face[0..model.handle.numface];
-        var indices_toreturn = std.ArrayList(u32).init(gpa);
-        for (indices) |index| {
-            try indices_toreturn.append(index.vertex[0]);
-            try indices_toreturn.append(index.vertex[1]);
-            try indices_toreturn.append(index.vertex[2]);
+        const faces: []model3d.Face = model.handle.face[0..model.handle.numface];
+        var buffer_toreturn = std.ArrayList(f32).init(gpa);
+        for (faces) |face| {
+            for (0..3) |i| {
+                try buffer_toreturn.append(vertices[face.vertex[i]].x);
+                try buffer_toreturn.append(vertices[face.vertex[i]].y);
+                try buffer_toreturn.append(vertices[face.vertex[i]].z);
+                try buffer_toreturn.append(vertices[face.normal[i]].x);
+                try buffer_toreturn.append(vertices[face.normal[i]].y);
+                try buffer_toreturn.append(vertices[face.normal[i]].z);
+            }
         }
 
         return .{
-            .vertices = vertices_toreturn.toOwnedSlice() catch unreachable,
-            .indices = indices_toreturn.toOwnedSlice() catch unreachable
+            .buffer = buffer_toreturn.toOwnedSlice() catch unreachable,
         };
     }
 };
